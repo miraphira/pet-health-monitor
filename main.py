9abb6bd1b4c5b5d5e147031cfa9c088fee2e4999
miraphira <fadhilahintan01@gmail.com>
2026-06-17 19:16:34 +0700

    Add header, weight chart, and improve dashboard

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from database import connect_db
 import random
+from datetime import datetime
 
 app = FastAPI()
 
@@ -11,8 +12,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+def format_date(date_str):
+    dt = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+    return dt.strftime("%d %b")
+
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
+    today_date = datetime.now().strftime("%d %b %Y")
 
     conn = connect_db()
     cursor = conn.cursor()
@@ -23,7 +29,65 @@ def home(request: Request):
     """)
 
     logs = cursor.fetchall()
-    latest_log = logs[0] if logs else None
+
+    cursor.execute("""
+        SELECT * FROM health_logs
+        ORDER BY created_at DESC LIMIT 5
+    """)
+    latest_logs = cursor.fetchall()
+
+    cursor.execute("""
+        SELECT * 
+        FROM health_logs 
+        WHERE type = 'berat badan'
+        ORDER BY created_at DESC
+        LIMIT 1
+    """)
+    last_weight_log = cursor.fetchone()
+    log_weight_dict = dict(last_weight_log)
+    weight_date = format_date(log_weight_dict["created_at"])
+
+    cursor.execute("""
+        SELECT weight, created_at 
+        FROM health_logs
+        WHERE type = 'berat badan' 
+        ORDER BY created_at
+    """)
+    weight_logs = cursor.fetchall()
+
+    jml_makan = 0
+    jml_muntah = 0
+    jml_bab = 0
+    jml_obat = 0
+    jml_bb = 0
+    log_list = []
+
+    for log in logs:
+        if log["type"] == "makan":
+            jml_makan += 1
+
+        elif log["type"] == "muntah":
+            jml_muntah += 1
+        
+        elif log["type"] == "BAB":
+            jml_bab += 1
+        
+        elif log["type"] == "obat":
+            jml_obat += 1
+        
+        elif log["type"] == "berat badan":
+            jml_bb += 1
+        
+        log_dict = dict(log)
+        log_dict["formatted_date"] = format_date(log_dict["created_at"])
+        log_list.append(log_dict)
+    
+    weight_dates = []
+    weight_values = []
+
+    for log in weight_logs:
+        weight_dates.append(format_date(log["created_at"]))
+        weight_values.append(log["weight"])
     conn.close()
 
     moods = [
@@ -35,16 +99,31 @@ def home(request: Request):
     "bola hitam menunggu makan"
     ]
 
-    moi_mood = random.choice(moods)
+    if jml_muntah > 5 :
+        moi_mood = "Moi sebaiknya ke dokter! 😭"
+    elif jml_muntah > 2:
+        moi_mood = "Moi berusaha fine 🥺"
+    else:
+        moi_mood = random.choice(moods)
 
     return templates.TemplateResponse(
         request= request,
         name= "index.html",
         context={
-        "logs": logs,
+        "today_date": today_date,
+        "logs": log_list,
         "total_logs": len(logs),
-        "latest_log": latest_log,
-        "mood_moi": moi_mood
+        "latest_log": latest_logs,
+        "last_weight": last_weight_log,
+        "weight_date": weight_date,
+        "weight_dates": weight_dates,
+        "weight_values": weight_values,
+        "mood_moi": moi_mood,
+        "jml_makan": jml_makan,
+        "jml_muntah": jml_muntah,
+        "jml_bab": jml_bab,
+        "jml_obat": jml_obat,
+        "jml_bb": jml_bb
         }
     )
 
@@ -76,7 +155,8 @@ def edit_page(request: Request, log_id: int):
 @app.post("/submit-log")
 def submit_log(
     log_type: str = Form(...),
-    note: str | None = Form(None)
+    note: str | None = Form(None),
+    weight: float | None = Form(None)
 ):
     print("===DATA BARU===")
     print("Jenis log: ", log_type)
@@ -87,10 +167,10 @@ def submit_log(
 
     cursor.execute(
         """
-        INSERT INTO health_logs (type, note)
-        VALUES (?, ?)
+        INSERT INTO health_logs (type, note, weight)
+        VALUES (?, ?, ?)
         """,
-        (log_type, note)
+        (log_type, note, weight)
     )
 
     conn.commit()
@@ -126,7 +206,7 @@ def delete_log(log_id: int):
 def update_log(
         log_id: int,
         log_type: str = Form(...),
-        note: str = Form(...)
+        note: str | None = Form(None)
 ):
     conn = connect_db()
     cursor = conn.cursor()
